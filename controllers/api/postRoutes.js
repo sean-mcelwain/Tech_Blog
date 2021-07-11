9eac9a8f7eae946c7cb0e08736ee389a3a270f5b
sean-mcelwain <79949547+sean-mcelwain@users.noreply.github.com>
2021-07-11 16:56:06 -0500

    added models for Users and Posts

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,63 @@
+const router = require('express').Router();
+const { Post } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+  try {
+
+    const newPost = await Post.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newPost);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: 'No blog post found with this id!' });
+      return;
+    }
+
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Updates book based on its book_id
+router.put('/:id', async (req, res) => {
+
+  const updatePost = await Post.update(
+    {
+      name: req.body.name,
+      description: req.body.description
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((updatedPost) => {
+      res.json(updatedPost);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json(err);
+    });
+});
+
+
+module.exports = router;
